74d5551a7e727508d9b6b87a0e3c908dc91c1aee
Milats <israestan@gmail.com>
2022-01-10 16:02:58 -0500

    Inicio del módulo Login en su respectiva branch

--- app/src/main/java/com/fisei/athanasiaapp/MainActivity.java
+++ app/src/main/java/com/fisei/athanasiaapp/MainActivity.java
@@ -52,9 +52,13 @@ public class MainActivity extends AppCompatActivity {
         protected JSONObject doInBackground(URL... params){
             HttpURLConnection connection = null;
             try{
+                //Realiza la conexión y petición.
                 connection = (HttpURLConnection) params[0].openConnection();
+                //Recibe un código de respuesta de la petición.
                 int response = connection.getResponseCode();
+                //Verifica que la petición haya sido un éxito
                 if(response == HttpURLConnection.HTTP_OK){
+                    //Lee el contenido de la petición.
                     StringBuilder builder = new StringBuilder();
                     try(BufferedReader reader = new BufferedReader(
                             new InputStreamReader(connection.getInputStream()))){
@@ -91,11 +95,10 @@ public class MainActivity extends AppCompatActivity {
     private void convertJSONtoArrayList(JSONObject forecast){
         productList.clear();
         try{
+            //El JSON que se recibe contiene los productos en el atributo "data"
             JSONArray list = forecast.getJSONArray("data");
             for(int i = 0; i < list.length(); ++i){
                 JSONObject products = list.getJSONObject(i);
-                //JSONObject temperatures = products.getJSONObject("temp");
-                //JSONObject weather = day.getJSONArray("weather").getJSONObject(0);
                 productList.add(new Product(
                         products.getString("id"),
                         products.getString("name"),
@@ -103,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
                         products.getInt("quantity"),
                         products.getDouble("unitPrice"),
                         products.getDouble("cost"),
-                        getString(R.string.icon_athanasia_url),
+                        getString(R.string.athanasia_icon_url),
                         "",
                         ""));
             }
